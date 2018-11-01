55bf7bac49ef9d716a5c0f514301e47380ab0578
Luc Saffre <luc.saffre@gmail.com>
2018-11-01 09:59:21 +0200

    http://luc.lino-framework.org/blog/2018/1101.html

--- lino_noi/lib/noi/models.py
+++ lino_noi/lib/noi/models.py
@@ -29,7 +29,7 @@ from lino.api import dd
 
 @dd.receiver(dd.post_analyze)
 def my_details(sender, **kw):
-    sender.modules.system.SiteConfigs.set_detail_layout("""
+    sender.models.system.SiteConfigs.set_detail_layout("""
     site_company next_partner_id:10 default_build_method
     site_calendar simulate_today hide_events_before
     default_event_type max_auto_events 
