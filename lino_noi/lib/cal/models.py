edd06ee9b570172962f18d4b1c0cf3697d14e7fc
Luc Saffre <luc.saffre@gmail.com>
2019-01-03 18:21:42 +0200

    http://luc.lino-framework.org/blog/2019/0103.html

--- lino_noi/lib/cal/models.py
+++ lino_noi/lib/cal/models.py
@@ -8,6 +8,71 @@
 from lino.api import dd, _
 
 from lino_xl.lib.cal.models import *
+from lino_xl.lib.working.choicelists import ReportingTypes
+from lino_xl.lib.working.ui import load_sessions, TOTAL_KEY
+
+
+# class Day(Day):
+#     def __init__(self, *args, **kwargs):
+#         super(Day, self).__init__(*args, **kwargs)
+#         self.sar = self.ar.spawn(rt.models.working.MySessionsByDay, master_instance=self)
+#         load_sessions(self, self.sar)
+#
+#
+# class Days(Days, dd.VentilatingTable):
+#
+#     # column_names_template = 'day_number long_date detail_link description {vcolumns}'
+#     column_names_template = 'detail_link worked_tickets {vcolumns} *'
+#
+#     @dd.displayfield(_("Tickets"))
+#     def worked_tickets(self, obj, ar):
+#         # pv = dict(start_date=obj.day, end_date=obj.day)
+#         # pv.update(observed_event=dd.PeriodEvents.active)
+#         # pv.update(user=ar.param_values.user)
+#         # sar = ar.spawn(MySessionsByDate, param_values=pv)
+#         # elems = [obj.sar.ar2button(label=six.text_type(obj))]
+#         elems = []
+#         tickets = [
+#             ar.obj2html(t, "#{0}".format(t.id), title=t.summary)
+#             for t in obj._tickets]
+#         if len(tickets) > 0:
+#             # elems.append(" (")
+#             elems += join_elems(tickets, ', ')
+#             # elems.append(")")
+#         return E.span(*elems)
+#
+#     # @dd.displayfield("Date")
+#     # def date(cls, row, ar):
+#     #     return dd.fdl(row.day)
+#
+#     @classmethod
+#     def param_defaults(cls, ar, **kw):
+#         kw = super(Days, cls).param_defaults(ar, **kw)
+#         kw.update(start_date=dd.today(-7))
+#         kw.update(end_date=dd.today())
+#         kw.update(user=ar.get_user())
+#         return kw
+#
+#     @classmethod
+#     def get_ventilated_columns(cls):
+#
+#         def w(rpttype, verbose_name):
+#             def func(fld, obj, ar):
+#                 return obj._root2tot.get(rpttype, None)
+#
+#             return dd.VirtualField(dd.DurationField(verbose_name), func)
+#
+#         for rpttype in ReportingTypes.objects():
+#             yield w(rpttype, six.text_type(rpttype))
+#         # yield w(None, _("N/A"))
+#         yield w(TOTAL_KEY, _("Total"))
+#
+#
+# class DayDetail(dd.DetailLayout):
+#     main = "working.MySessionsByDay cal.PlannerByDay"
+#
+#
+
 
 class Event(Event, ContactRelated):
     class Meta:
