34d71dd560253b0f830cd4417f7ef319ed71d0f4
Luc Saffre <luc.saffre@gmail.com>
2020-07-14 11:47:45 +0300

    http://luc.lino-framework.org/blog/2020/0714.html

--- lino_noi/__init__.py
+++ lino_noi/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014-2018 Rumma & Ko Ltd
+# Copyright 2014-2020 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 """This is the main module of Lino Noi.
 
@@ -11,14 +11,10 @@
 
 """
 
-from os.path import join, dirname
-fn = join(dirname(__file__), 'setup_info.py')
-exec(compile(open(fn, "rb").read(), fn, 'exec'))
+from lino_noi.setup_info import SETUP_INFO
 
 __version__ = SETUP_INFO['version']
 
 srcref_url = 'https://github.com/lino-framework/noi/blob/master/%s'
 intersphinx_urls = dict(docs="http://noi.lino-framework.org")
 doc_trees = ['docs']
-
-
